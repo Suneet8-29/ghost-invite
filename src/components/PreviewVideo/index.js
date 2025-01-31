cbb82d7c28798791403d7a17d89cb83e9b9e68fb
Harshil Shah <harshil.shah@velotio.com>
2025-01-31 16:33:15 +0530

    image fix

--- src/components/PreviewVideo/index.js
+++ src/components/PreviewVideo/index.js
@@ -1,10 +1,16 @@
 import "./style.css";
+import previewImage from "../../images/preview.webp";
 
 const PreviewVideo = ({ title = "Sample title" }) => {
   return (
     <div className="previewContainer">
       <h4 className="previewTitle">{title}</h4>
-      <video className="previewContent" controls width="100%">
+      <video
+        className="previewContent"
+        controls
+        playsinline
+        poster={previewImage}
+      >
         <source
           src="https://firebasestorage.googleapis.com/v0/b/tripimagestore-57c72.appspot.com/o/videos%2FWedding%2FSarita%20Suneet%20Prewedding.mp4?alt=media&token=48050b0e-7208-4d30-a0a8-2eb72054e6db"
           type="video/mp4"
